a92c904b80cf104e9b2d80994851eadfece3ed99
Leandro Souza <leandroades@gmail.com>
2017-10-22 16:36:57 -0200

    Ajustes estrutura do projeto

--- SpringBootJerseyEx/src/main/java/net/leandroabreu/springbootjerseyex/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package net.leandroabreu.springbootjerseyex;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ContatoRepository extends PagingAndSortingRepository<Contato, Long> {
-
-}
